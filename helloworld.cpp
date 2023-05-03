3b9392f26e8303e4758ae819edf94bbf05c6390e
HHansen6534 <59617097+HHansen6534@users.noreply.github.com>
2023-05-03 13:05:59 -0700

    Create helloworld.cpp
    test c++ file

---
+++ helloworld.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    string name;
+    string response;
+    cout << "Hello World!" << endl;
+    cout << "What is your name?" << endl;
+    cin >> name; 
+    cout << "Well hello " << name << "! Welcome to this github repository! Are you admin or a developer? (A/D)";
+    cin >> response; 
+    
+    if (response == "A"){
+        cout << " Welcome to the admin team!";
+    }
+    else {
+        cout << "Welcome to the developer team!";
+    }
+
+
+}
