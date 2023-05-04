b3463e996a5c0a4902488d2c167058510b99e56c
HHansen6534 <59617097+HHansen6534@users.noreply.github.com>
2023-05-04 13:23:13 -0700

    Create calculator.cpp
    new calc file

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int firstnum;
+  int secondnum;
+  int solutiom;
+ cout << "Please input a number!" <<endl;
+ cin >> firstnum << endl;
+  cout << "Please enter your second number!" << endl;
+  cin >> secondnum;
+  solution = secondnum + firstnum;
+  cout << "The answer to your solution is: " << solution << endl;
+}
