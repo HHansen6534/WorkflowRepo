404f449b08b9d5e01de6a58fd80de3d0a996dd33
HHansen6534 <haleyhansen6534@gmail.com>
2023-05-04 21:43:19 +0000

    Format C++ code using clang-format

--- color.cpp
+++ color.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-int main () {
+int main() {
   string color;
- cout << "Hello! what is your favorite color?" << endl;
+  cout << "Hello! what is your favorite color?" << endl;
   cin >> color >> endl;
   cout << "Wow! The color " << color << " is so pretty, good choice!" << endl;
 }
--- multiplier.cpp
+++ multiplier.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int main(){
-cout << "please enter a number!" << endl;
-int num1;
-cin >>  num1 >> endl;
-cout << "please enter a second number!" << endl;
-cin >> num 2 >> endl;
-int num2;
-solution = num1 * num2;
-cout << "This is your number after being multiplied by one another! number: " << solution << endl;
+int main() {
+  cout << "please enter a number!" << endl;
+  int num1;
+  cin >> num1 >> endl;
+  cout << "please enter a second number!" << endl;
+  cin >> num 2 >> endl;
+  int num2;
+  solution = num1 * num2;
+  cout << "This is your number after being multiplied by one another! number: "
+       << solution << endl;
 }
