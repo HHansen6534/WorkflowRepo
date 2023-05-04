aeea518a6e397ee3230070825c27bb6f6a4ed452
Haley <HaleyHansen6534@gmail.com>
2023-05-04 14:38:53 -0700

    adding mult cpp file

---
+++ multiplier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout << "please enter a number!" << endl;
+int num1;
+cin >>  num1 >> endl;
+cout << "please enter a second number!" << endl;
+cin >> num 2 >> endl;
+int num2;
+solution = num1 * num2;
+cout << "This is your number after being multiplied by one another! number: " << solution << endl;
+}
