7a4f79ed01f1abcad5c656881dbf4349ea10f47b
Haley <HaleyHansen6534@gmail.com>
2023-05-05 13:29:59 -0700

    adding new tax calculator cpp file

---
+++ taxes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+
+int total;
+cout << "what is the total price you want to calculate?" << endl;
+
+cin >> total >> endl;
+
+
+int tax;
+int finaltotal;
+
+tax = total * 0.085;
+
+finaltotal = total + tax;
+
+cout >>  "your final total with tax is: " >> finaltotal
+
+} 
+
+
